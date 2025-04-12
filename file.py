f05aaa7065246d500a5557b884ad0179ffb4534c
VamsiMors <vamsimors@gmail.com>
2025-04-12 12:45:34 +0100

    test

---
+++ file.py
@@ -0,0 +1,9 @@
+# with open('/home/vjo04/gitdemo-1/write.txt','w') as f:
+#     f1=open('/home/vjo04/gitdemo-1/test.txt','r')
+#     for i in f1.read():
+#         n=print(i,end="")
+#         f.write("n")
+#         print(f)
+w=open('/home/vjo04/gitdemo-1/write.txt','r')
+n=w.readlines()
+print(n)
